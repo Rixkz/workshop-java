a73336078e5b1b9090bfa9cdb4f7d53d20246d50
Adisorn Suwanaium <adisorn.suw@turbo.co.th>
2021-07-23 14:55:15 +0700

    fix naming test case

--- src/test/java/GenerateRangeTest.java
+++ src/test/java/GenerateRangeTest.java
@@ -4,14 +4,14 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class GenerateRangeTest {
     @Test
-    @DisplayName("Test input [1,5] start must be [ (include symbol)")
+    @DisplayName("Test input [1,5] start symbol be [ (include symbol)")
     public void startWithIncludeSymbol() {
         GenerateRange generateRange = new GenerateRange("[1,5]");
         boolean validateResult = generateRange.isStartWithInclude();
         assertTrue(validateResult);
     }
     @Test
-    @DisplayName("Test input (1,5] start must be ( (exclude symbol)")
+    @DisplayName("Test input (1,5] start symbol be ( (exclude symbol)")
     public void startWithExcludeSymbol() {
         GenerateRange generateRange = new GenerateRange("(1,5]");
         boolean validateResult = generateRange.isStartWithInclude();
